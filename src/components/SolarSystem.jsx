1f2663ae91882b13a51760d007841dbb641c7ff1
Cristiano Vieira <cristianoviieira@gmail.com>
2022-04-07 01:54:01 -0300

    requisito 5 finalizado

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import Title from './Title';
-
-const headline = {
-  nome: 'Planetas',
-};
+import PlanetCard from './PlanetCard';
+// import planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
-        <Title headline={ headline.nome } />
+        <Title headline="Planetas" />
+        <PlanetCard planetName="Nome" planetImage="https://t2.ea.ltmcdn.com/pt/posts/9/7/3/cachorro_pode_comer_maca_22379_orig.jpg" />
       </div>
     );
   }
