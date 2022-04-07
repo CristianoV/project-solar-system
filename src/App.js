7883b046b3eda976ccbae6ba7e5436349e9ed976
Cristiano Vieira <cristianoviieira@gmail.com>
2022-04-06 21:20:08 -0300

    terminando requisito 3

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import Title from './components/Title';
 
+const headline = {
+  nome: 'Planetas',
+};
+
 class App extends React.Component {
   render() {
     return (
       <>
         <Header />
         <SolarSystem />
-        <Title />
+        <Title headline={ headline.nome } />
       </>
     );
   }
